5dfb89e9f4969f79616eb84e2d2899408307b759
try ghoul <ggsanky@gmail.com>
2026-01-03 18:50:48 +0300

    setting

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
